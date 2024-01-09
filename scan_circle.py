c9f76a291f006c440df4b47fa340dc607f62d13d
Jamie Alison <au694732@uni.au.dk>
2024-01-09 16:01:22 +0100

    Converted to a nice script that creates a mask for all jpg files in a folder, and then extracts the circle from the final mask.

--- scan_circle.py
+++ scan_circle.py
@@ -7,39 +7,32 @@ Created on Tue Jan  9 11:29:55 2024
 Remember to:
 
 pip install miniball
+pip install opencv-python
 
-As conda cannot install this package.
+As conda cannot install those packages.
 
 """
 
-import numpy as np, miniball, math, cv2, matplotlib.pyplot as plt,scipy.ndimage
+import numpy as np, miniball, math, cv2, scipy.ndimage, os
 
 wd="O:/Tech_ECOS-OWF-Screening/Fugle-flagermus-havpattedyr/BIRDS/Ship_BasedSurveys/VerticalRadar/ScreenDumps/2023 08 10-16/"
 
-# Load the image
-image = cv2.imread(wd+"CAP_20230810_215849_287.jpg")
- 
-# Display the image
-cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-cv2.imshow("Image", image)
-cv2.waitKey()
+# List all files in the directory ending with ".jpg"
+jpg_files = [f for f in os.listdir(wd) if f.lower().endswith(".jpg")]
 
-mask = np.all((image >= [100,0,0]) & (image <= [120,5,5]), axis=-1)
-#convert boolean array to binary image to display
-mask_disp=(mask * 255).astype(np.uint8)
-cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-cv2.imshow("Image", mask_disp)
-cv2.waitKey()
+files=jpg_files[0:100]
 
+for file in files:
 
-
-
-
-print(image[1000,1000])
-
-
-
-
+    # Load the image
+    image = cv2.imread(wd+file)
+    
+    this_mask = np.all((image >= [100,0,0]) & (image <= [120,5,5]), axis=-1)
+    if file==files[0]:
+        mask = np.all((image >= [100,0,0]) & (image <= [120,5,5]), axis=-1)
+    mask = mask | this_mask
+    
+    
 # b=image[:, :, 0]
 # r=image[:, :, 2]
 
@@ -47,7 +40,7 @@ print(image[1000,1000])
 # ret,scanb = cv2.threshold(b,100,120,cv2.THRESH_BINARY)
 # ret,scanr = cv2.threshold(r,0,5,cv2.THRESH_BINARY)
 
-
+#need to shrink and extract only the edge pixels to avoid memory error when finding minimum bounding circle
 mask_shrink=scipy.ndimage.binary_erosion(mask)
 mask_edge=mask!=mask_shrink
 mask_edge_disp=(mask_edge * 255).astype(np.uint8)
@@ -78,5 +71,11 @@ cv2.waitKey()
 
 output_path = wd+"an_output_image.jpg"  # Replace with the desired output path and file name
 
+#convert boolean array to binary image to display
+mask_disp=(mask * 255).astype(np.uint8)
+cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+cv2.imshow("Image", mask_disp)
+cv2.waitKey()
+
 # Save the image as JPEG
-cv2.imwrite(output_path, image)
+cv2.imwrite(output_path, mask_disp)
